206c842e66d20061ad98fe3c63ddd2301a209418
mariorobles93 <mario.robles.93@gmail.com>
2023-11-07 21:34:34 -0600

    SM-13 : Date and Time Component (#5)
    * SM-13: Added test configuration
    
    * SM-13: Added back lock.json
    
    * SM-13: Added ts-node
    https://stackoverflow.com/questions/40910864/cannot-find-module-ts-node-register
    
    * SM-13: changed angular.json
    
    * changed file locations
    
    * changed jest config
    
    * changed package.json
    
    * Sm-13: Added Time and Date Component
    
    * Sm-13: Fixed tests
    
    * Sm-13: Fixed tests
    
    * Sm-13: Fixed tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {DateAndTimeComponent} from "./date-and-time/date-and-time.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DateAndTimeComponent
   ],
   imports: [
     BrowserModule,
