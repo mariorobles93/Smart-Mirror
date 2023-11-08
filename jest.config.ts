a4490b2555a6368a233ca006ef56b9993a8189cc
Mario Robles <mario.robles.93@gmail.com>
2023-11-07 20:47:22 -0600

    changed jest config

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,6 @@ module.exports = {
     transform: {
         "^.+\\.ts?$": "ts-jest",
     },
-    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.ts$",
     moduleFileExtensions: ["ts", "js", "jsx", "json", "node"],
     collectCoverage: true,
     coverageThreshold: {
