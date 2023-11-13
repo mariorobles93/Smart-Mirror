4aa573cec3a2f2c1a6f6bd1140dc2e19b8c55f84
Mario Robles <mario.robles.93@gmail.com>
2023-11-13 13:36:25 -0600

    SM-13: removed unused code
    Signed-off-by: Mario Robles <mario.robles.93@gmail.com>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,9 @@
 import {TestBed} from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
 import {AppComponent} from './app.component';
-import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
-import {HttpClient} from "@angular/common/http";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('AppComponent', () => {
-    let httpClient = HttpClient;
-    let httpTestingController: HttpTestingController;
-
     beforeEach(() => TestBed.configureTestingModule({
         imports: [
             RouterTestingModule,
